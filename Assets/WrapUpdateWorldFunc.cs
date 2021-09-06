f81cf51875ab0dcbacbd702dd1fc7e9d182f8386
Ben Mora <blackopsben@gmail.com>
2021-09-05 22:55:42 -0500

    World wrapping working!

---
+++ Assets/WrapUpdateWorldFunc.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrapUpdateWorldFunc : MonoBehaviour
+{
+    public void UpdateWorldWrap()
+    {
+        Debug.Log("It worked!");
+    }
+}
